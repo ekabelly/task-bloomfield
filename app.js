28baedbfc14ee0fdcb4e55ca6aa860ae2b97bc7b
Eedoh <eedohk@matrix.co.il>
2020-02-13 17:44:37 +0200

    feat(db): added connection

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+
+const conn = require('./db/conn');
+
 app.use(bodyParser.json());
+
 
 let Test = 0;
 
@@ -10,4 +14,10 @@ app.get('/test', (req, res, next) => {
     res.status(200).send({Test});
 });
 
-app.listen(3001, () => console.log(`test server up and running on port 3001`));
+conn.connect(err => {
+    if(err){
+        console.error(err);
+    } else {
+        app.listen(3001, () => console.log(`test server up and running on port 3001`));
+    }
+})
