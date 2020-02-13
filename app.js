7f7b21528d7820fd874446a547f0cf44caaaed70
Eedoh <eedohk@matrix.co.il>
2020-02-13 17:13:26 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+let Test = 0;
+
+app.get('/test', (req, res, next) => {
+    Test++;
+    res.status(200).send({Test});
+});
+
+app.listen(3001, () => console.log(`test server up and running on port 3001`));
