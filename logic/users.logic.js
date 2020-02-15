8ebefd1bfde8d60800c48b1a2cb5a36b60cdfbff
Eedoh <ekabelly@gmail.com>
2020-02-15 15:59:17 +0200

    added comments

--- logic/users.logic.js
+++ logic/users.logic.js
@@ -4,13 +4,20 @@ const util = require('../util/app-util');
 const messagesLogic = require('./messages.logic');
 
 const handleUsersCreation = async users => {
-    const messageId = util.makeUniqueId();
-    await insertUsers(
-        util.transformUsersObjToArr(users, messageId)
-    );
-    await insertMessage([messageId, new Date(), false]);
-    messagesLogic.handleMessageLogic(messageId);
-    return true;
+    if(users && users.length > 0){
+        const messageId = util.makeUniqueId();
+        await insertUsers(
+            // transform users arr (of objects) to users arr of arrays and add messageId to each user.
+            util.transformUsersObjToArr(users, messageId)
+        );
+        // create a message with was_sent is false
+        await insertMessage([messageId, new Date(), false]);
+        // will send message and update in db
+        messagesLogic.handleMessageLogic(messageId);
+        return true;
+    }
+    // throw an error if no users were given.
+    throw new Error('no users were given.');
 }
 
 module.exports = {
